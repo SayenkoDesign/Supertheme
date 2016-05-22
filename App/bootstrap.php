2e453bfd963bb85e909b4002a2b3caed031b642c
Robert M. Parker <rparker@yamiko.ninja>
2016-05-22 10:14:51 -0700

    fixed config variable teypo

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -6,7 +6,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 use Symfony\Component\Config\FileLocator;
 
 $container = new ContainerBuilder();
-$container->setParameter('tempalte_dir', get_template_directory());
+$container->setParameter('template_dir', get_template_directory());
 $container->setParameter('WP_DEBUG', WP_DEBUG);
 
 $loader = new YamlFileLoader($container, new FileLocator(get_template_directory()));
