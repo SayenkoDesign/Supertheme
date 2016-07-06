558ebcdd90118374c32c88ee194cd357bd8b4cea
Robert M. Parker <rparker@yamiko.ninja>
2016-07-06 00:24:58 -0700

    added additional walkers

---
+++ src/classes/WordPress/DropDownMenuWalker.php
@@ -0,0 +1,12 @@
+<?php
+namespace Supertheme\WordPress;
+
+use Walker_Nav_Menu;
+
+class DropDownMenuWalker extends Walker_Nav_Menu
+{
+    public function start_lvl( &$output, $depth = 0, $args = array() ) {
+        $indent = str_repeat("\t", $depth);
+        $output .= "\n$indent<ul class=\"menu\">\n";
+    }
+}
