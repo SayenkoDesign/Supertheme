c23d5033c6c24f2fb06d707a83364faf7b6b80dd
Robert M. Parker <rparker@yamiko.ninja>
2016-09-14 14:24:11 -0700

    add walkers and uri as global

--- App/bootstrap.php
+++ App/bootstrap.php
@@ -12,3 +12,10 @@ $container->setParameter('WP_DEBUG', WP_DEBUG);
 
 $loader = new YamlFileLoader($container, new FileLocator(get_template_directory()));
 $loader->load('app/config/config.yml');
+
+$twig->addGlobal('url', get_site_url());
+$twig->addGlobal('walkers', [
+    'accordion' => new \Supertheme\WordPress\AccordionMenuWalker(),
+    'drilldown' => new \Supertheme\WordPress\DrillDownMenuWalker(),
+    'dropdown' => new \Supertheme\WordPress\DropDownMenuWalker(),
+]);
