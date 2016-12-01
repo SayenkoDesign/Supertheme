be85de35ae7a5d648d8ba727c0747ef02ec1af5a
Robert M. Parker <rparker@yamiko.ninja>
2016-11-30 23:38:41 -0800

    update php libs

--- functions.php
+++ functions.php
@@ -1,3 +1,10 @@
 <?php
 require_once __DIR__.'/app/bootstrap.php';
 require_once __DIR__.'/src/functions.php';
+
+add_filter('timber/context', function($data){
+    // logos
+    $data['menu'] = new Timber\Menu('primary_menu');
+
+    return $data;
+});
