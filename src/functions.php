c202a553245023d55dc7aa140de65b568809929b
Robert Parker <rparker@kodama.design>
2017-03-21 15:06:20 -0700

    dont replace jquery on login or registration pages

--- src/functions.php
+++ src/functions.php
@@ -52,7 +52,7 @@ add_action('init', function () use($container) {
     }
 
     // replace jquery
-    if (!is_admin()) {
+    if (!is_admin() && !in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'))) {
         wp_deregister_script('jquery');
         wp_register_script('jquery', 'https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js', false, '2.2.4');
         wp_enqueue_script('jquery');
